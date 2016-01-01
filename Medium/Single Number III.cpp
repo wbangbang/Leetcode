eb47e2fa5499f51d600d1f779a210ab859f0a2d5
wbangbang <wbangbang11@sina.com>
2015-12-31 21:34:24 -0800

    Bit manipulation

--- Medium/Single Number III.cpp
+++ Medium/Single Number III.cpp
@@ -17,3 +17,26 @@ public:
         return r;
     }
 };
+
+class Solution {//bit manipulation
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int m = 0;
+        for(int i = 0; i < nums.size(); i++) {//due to single number x1, x2, m != 0, so m must has one bit which is not 0.
+            m ^= nums[i];
+        }
+        int flag = m & (~(m -1)), a = 0, b = 0;//find the lowest bit which is 1.
+        for(int i = 0; i < nums.size(); i++) {
+            if(flag & nums[i]) {
+                a ^= nums[i];
+            }
+            else {
+                b ^= nums[i];
+            }
+        }
+        vector<int> res;
+        res.push_back(a);
+        res.push_back(b);
+        return res;
+    }
+};
