5764417b86ebc1ea6130eca82c5ddff25350d729
wbangbang <wbangbang11@sina.com>
2016-01-05 22:24:21 -0800

    Binary Search

--- Medium/Find Minimum in Rotated Sorted Array.cpp
+++ Medium/Find Minimum in Rotated Sorted Array.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        vector<int> n;
-        int mid;
-        if(nums.size() == 1) return nums[0];
-        if(nums.size() == 2) return min(nums[0], nums[1]);
-        if(nums[0] < nums[nums.size() - 1]) return nums[0];
-        if(nums.size() % 2 == 0) mid = nums.size() / 2;
-        else mid = (nums.size() - 1) / 2;
-        int t = nums[mid];
-        if(t > nums[0]) {
-            for(int i = mid + 1; i < nums.size(); i++) {
-                n.push_back(nums[i]);
+        if(nums.empty()) return -1;
+        int n = nums.size(), left = 1, right = n - 1, min = nums[0];
+        if(n == 1) return nums[0];
+        if(nums[0] < nums[n - 1]) return nums[0];
+        while(left <= right) {
+            int mid = (left + right) >> 1;
+            if(nums[mid] > min) {
+                left = mid + 1;
             }
-            return findMin(n);
-        }
-        else {
-            for(int i = 0; i < mid + 1; i++) {
-                n.push_back(nums[i]);
+            else {
+                min = nums[mid];
+                right = mid - 1;
             }
-            return findMin(n);
         }
-    }
-    
-    int min(int x, int y) {
-        if(x < y) return x;
-        else return y;
+        return min;
     }
 };
