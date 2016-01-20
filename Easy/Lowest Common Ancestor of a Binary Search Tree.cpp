cacc56adce965131dd47a13b2b1ac21964045160
wbangbang <wbangbang11@sina.com>
2016-01-19 22:28:23 -0800

    Tree

--- Easy/Lowest Common Ancestor of a Binary Search Tree.cpp
+++ Easy/Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -7,7 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {
+class Solution {//iterative
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         int max = maxV(p, q), min = minV(p, q);
@@ -33,5 +33,19 @@ public:
     int minV(TreeNode* p, TreeNode* q) {
         if(p->val < q->val) return p->val;
         else return q->val;
+    }
+};
+
+class Solution {//recursive
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root) return root;
+        if(root->val <= max(p->val, q->val) && root->val >= min(p->val, q->val)) return root;
+        else if(root->val < min(p->val, q->val)) {
+            return lowestCommonAncestor(root->right, p, q);
+        }
+        else {
+            return lowestCommonAncestor(root->left, p, q);
+        }
     }
 };
